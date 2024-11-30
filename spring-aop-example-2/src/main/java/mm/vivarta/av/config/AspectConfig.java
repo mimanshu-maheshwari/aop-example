957b36e39b46fc275e2d7d485a4de884d5e441ae
MIMANSHU MAHESHWARI <maheshwarimimanshu@gmail.com>
2024-11-30 20:33:33 +0530

    Changes

--- spring-aop-example-2/src/main/java/mm/vivarta/av/config/AspectConfig.java
+++ spring-aop-example-2/src/main/java/mm/vivarta/av/config/AspectConfig.java
@@ -1,8 +1,7 @@
 package mm.vivarta.av.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
-@EnableAspectJAutoProxy
-public class AspectConfig { }
+//@EnableAspectJAutoProxy
+public class AspectConfig {}
